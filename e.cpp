1e2cf06cc28056582a75539798898c687f9012a9
INDavid04 <davidirimia04@gmail.com>
2025-03-03 19:02:33 +0200

    test with redeclaration of a function

---
+++ e.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+// Asa da
+int f(int x);
+
+int f(int x) {
+    return x + 2;
+}
+*/
+
+/*
+// Asa nu
+int f(int x) {
+    return x + 1;
+}
+
+int f(int x) {
+    return x + 2;
+}
+*/
+
+int main() {
+    cout << f(3) << endl;
+
+    return 0;
+}
