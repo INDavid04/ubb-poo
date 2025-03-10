3cc2e532ad0c27f79dc5c2a7b81243563c0adf4b
INDavid04 <davidirimia04@gmail.com>
2025-03-10 14:55:17 +0200

    add the tasks for first seminary assignment

--- tema-seminar-1.cpp
+++ tema-seminar-1.cpp
@@ -1,5 +1,27 @@
-// Seminar #1 - Tema 1 Due March 18, 2025 11:59 PM
-// De implementat cele două clase Vector și Stivă de la finalul Seminarului I. Aveți seminarul atașat în canalul Seminar > Files > Seminarii > Seminarul I. Multă baftă.
+/// Seminar #1 - Tema 1 Due March 18, 2025 11:59 PM
+
+/// TODO (Mandatory):
+/// De implementat cele două clase Vector și Stivă de la finalul Seminarului I. Aveți seminarul atașat în canalul Seminar > Files > Seminarii > Seminarul I. Multă baftă.
+/// 1. Implementati clasa Stiva avand urmatoarele functionalitati:
+///     • memorie alocata dinamic
+///     • constructor (de toate tipurile) si destructor
+///     • metoda de adaugare element in Stiva
+///     • metoda de eliminare element din Stiva care returneaza elementul eliminat;
+/// 2. Implementati clasa Vector avand urmatoarele functionalitati:
+///     • memorie alocata dinamic
+///     • constructor (de toate tipurile) si destructor
+///     • metoda de adaugare element la sfarsitul vectorului
+///     • metoda de eliminare element de la sfarsitul vectorului
+///     • metoda ce intoarce un element de pe pozitia i primita ca parameteru
+///     • metoda ce intoarce numarul de elemente din vector
+
+
+/// TODO (Nice):
+/// 1. Implementati un program care citeste doua numere intregi de la tastatura si afiseaza maximul.
+/// 2. Implementati un program care citeste de la tastatura un n ≥ 3 si afiseaza al n-lea numar al lui Fibbonaci.
+/// 3. Implementati un program care citeste de la tastatura un vector de numere in virgula mobila si il sorteaza in ordine crescatoare.
+/// 4. Implementati un program care citeste de la tastatura un numar intreg si determina daca este palindrom.
+
 
 #include <iostream>
 #include <string.h>
