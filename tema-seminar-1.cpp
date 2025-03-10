738f3711d28c016604d9b0e39ba5386812967546
INDavid04 <davidirimia04@gmail.com>
2025-03-10 14:38:42 +0200

    declaring an array with type name[] {first, second, etc}

--- tema-seminar-1.cpp
+++ tema-seminar-1.cpp
@@ -1,18 +1,14 @@
 // Seminar #1 - Tema 1 Due March 18, 2025 11:59 PM
 // De implementat cele două clase Vector și Stivă de la finalul Seminarului I. Aveți seminarul atașat în canalul Seminar > Files > Seminarii > Seminarul I. Multă baftă.
 
-# include <iostream>
+#include <iostream>
+#include <string.h>
+
 using namespace std;
 
 int main() {
     int v[] {1, 2, 3, 4};
-    int i = 0;
-    while(v[i] != '\0'){
-        cout << v[i] << endl;
-        i++;
-    }
-    int n = i;
-    cout << "Lungimea vectorului este " << n << endl;
+    cout << "Primul element este " << v[0] << endl;
 
     return 0;
 }
