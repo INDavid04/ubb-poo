607ffc189000b0401d0730fe05d38412aa483587
INDavid04 <davidirimia04@gmail.com>
2025-03-14 18:00:44 +0200

    clasa student cu memorie alocata dinamic

--- tema-seminar-1.cpp
+++ tema-seminar-1.cpp
@@ -6,7 +6,12 @@
 
 using namespace std;
 
-/// TODO (Mandatory):
+///------------------------------------------------------------------------------------------------------------------///
+
+/// TODO (Mandatory, sursa: seminarul 1):
+
+///------------------------------------------------------------------------------------------------------------------///
+
 /// De implementat cele două clase Vector și Stivă de la finalul Seminarului I. Aveți seminarul atașat în canalul Seminar > Files > Seminarii > Seminarul I. Multă baftă.
 
 /// 1. Implementati clasa Stiva avand urmatoarele functionalitati:
@@ -31,15 +36,24 @@ using namespace std;
 ///     • metoda ce intoarce un element de pe pozitia i primita ca parameteru
 ///     • metoda ce intoarce numarul de elemente din vector
 
+///------------------------------------------------------------------------------------------------------------------///
 
-/// TODO (Nice):
+/// TODO (Nice, sursa: seminarul 1):
+
+///------------------------------------------------------------------------------------------------------------------///
+
 /// 1. Implementati un program care citeste doua numere intregi de la tastatura si afiseaza maximul.
 /// 2. Implementati un program care citeste de la tastatura un n ≥ 3 si afiseaza al n-lea numar al lui Fibbonaci.
 /// 3. Implementati un program care citeste de la tastatura un vector de numere in virgula mobila si il sorteaza in ordine crescatoare.
 /// 4. Implementati un program care citeste de la tastatura un numar intreg si determina daca este palindrom.
 
+///------------------------------------------------------------------------------------------------------------------///
 
-/// TODO (Aside):
+/// TODO (Aside, sursa: chatgpt):
+
+///------------------------------------------------------------------------------------------------------------------///
+
+/// Etapa 1: Bazele OOP - Clase și Obiecte
 
 class Persoana {
 private:
@@ -74,7 +88,7 @@ private:
     char model[40];
     int anFabricatie;
 public:
-    /// Constructorul
+    /// Constructor
     Masina(const char* marcaInitiala = "Seat", const char* modelInitial = "Leon", int anFabricatieInitial = 1950) {
         strcpy(marca, marcaInitiala);
         strcpy(model, modelInitial);
@@ -101,9 +115,111 @@ void clasa_masina() {
     m1.afiseazaDetalii();
 }
 
+///------------------------------------------------------------------------------------------------------------------///
+
+/// Etapa 2: Constructori, Destructor, Alocare Dinamică
+
+class Student {
+private:
+    char nume[20];
+    float* note = new float[5];
+    int nrNote;
+protected:
+public:
+    /// Constructor
+    Student(const char* numeInitial = "John Doe", const float* noteInitiale = nullptr, int nrNoteInitial = 3) {
+        strcpy(nume, numeInitial);
+        nrNote = nrNoteInitial;
+        note = new float [nrNoteInitial];
+
+        if (noteInitiale != nullptr) {
+            for (int i = 0; i < nrNoteInitial; i++) {
+                note[i] = noteInitiale[i];
+            }
+        } else {
+            for (int i = 0; i < nrNoteInitial; i++) {
+                note[i] = 0;
+            }
+        }
+    }
+
+    /// Destructor
+    ~Student() {
+        delete[] note;
+        cout << "Studentul " << nume << " a iesit din registru.\n";
+    }
+
+    /// Metoda de afișare a notelor
+    void afiseazaNote() {
+        cout << "Notele studentului " << nume << ": ";
+        for (int i = 0; i < nrNote; i++) {
+            cout << note[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+void implementarea_unei_clase_student_cu_memoria_alocata_dinamic() {
+    /// Atribute: nume, note (vector alocat dinamic de n elemente).
+    /// Constructor care primește un număr de note și le inițializează cu valori.
+    /// Destructor care eliberează memoria.
+    /// Metodă afiseazaNote().
+    /// Testează crearea și ștergerea obiectelor.
+    float noteExemplu[] = {7.8, 9.0, 3.5};
+    Student s1("Mihai Cristian", noteExemplu, 3);
+    Student s2("Andrei Popescu");
+
+    s1.afiseazaNote();
+    s2.afiseazaNote();
+}
+
+///------------------------------------------------------------------------------------------------------------------///
+
+void clasa_biblioteca_cu_array_alocat_dinamic() {
+    /// Conține un vector de cărți alocat dinamic și o metodă adaugaCarte().
+    /// Destructor care eliberează memoria.
+}
+
+/// Etapa 3: Encapsulare și Getteri/Setteri
+
+void clasa_cont_bancar() {
+    /// Atribute private: titular, sold.
+    /// Metode depunere(float), retrage(float), afiseazaSold().
+    /// Getteri și setteri pentru titular.
+}
+
+///------------------------------------------------------------------------------------------------------------------///
+
+void clasa_telefon() {
+    /// Atribute private: brand, pret.
+    /// Metode pentru modificarea prețului și afișarea detaliilor.
+}
+
+/// Etapa 4: Relații între clase (Compoziție, Agregare, Moștenire)
+
+void clasa_animal_si_mostenire() {
+    /// Clasa Animal cu atribut nume.
+    /// Clasa Caine derivată din Animal, care adaugă un atribut rasa.
+    /// Suprascrie metoda afisare().
+}
+
+///------------------------------------------------------------------------------------------------------------------///
+
+void clasa_care_si_biblioteca_compozitie() {
+    /// Carte are titlu, autor.
+    /// Biblioteca conține un vector de Carte.
+}
+
+///------------------------------------------------------------------------------------------------------------------///
+
+/// Functia main. Comenteaza/decomenteaza functiile care te intereseaza!
+
+///------------------------------------------------------------------------------------------------------------------///
+
 int main() {
     // definirea_unei_clase_persoana();
-    clasa_masina();
+    // clasa_masina();
+    implementarea_unei_clase_student_cu_memoria_alocata_dinamic();
 
     return 0;
 }
