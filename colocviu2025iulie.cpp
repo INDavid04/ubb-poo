c6d534e1a8471fd33f37226c144bd509062e948c
INDavid04 <davidirimia04@gmail.com>
2025-08-16 11:20:31 +0300

    add: colocviu2025iulie.cpp

---
+++ colocviu2025iulie.cpp
@@ -0,0 +1,285 @@
+/// 2025-08-16-09-45-11-20
+/// 5 minutes over time
+/// a lot of errors
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+/// Clasa abstracta
+class Intrebare {
+private:
+protected:
+    static int idIntrebareGenerator;
+    int idIntrebare;
+    string enunt, raspunsCorect;
+    list<string> listaTaguri;
+public:
+    Intrebare(const string &enunt, const string &raspunsCorect, const list<string> &listaTaguri) {
+        this->enunt = enunt;
+        this->raspunsCorect = raspunsCorect;
+        this->listaTaguri = listaTaguri;
+        idIntrebare = ++idIntrebareGenerator;
+    }
+
+    /// Pentru intrebarea libera nu avem raspuns corect
+    Intrebare(const string &enunt, const list<string> &listaTaguri) {
+        this->enunt = enunt;
+        this->listaTaguri = listaTaguri;
+        idIntrebare = ++idIntrebareGenerator;
+    }
+
+    virtual ~Intrebare() {
+        cout << "info: Intrebare distrusa\n";
+    }
+
+    virtual void afiseazaIntrebare() const = 0; /// functie virtuala pura
+};
+
+int Intrebare::idIntrebareGenerator = 0;
+
+class IntrebareGrila : public Intrebare {
+private:
+    const int numarOptiuniDeRaspuns;
+    const list<string> listaOptiuniDeRaspuns;
+protected:
+public:
+    // IntrebareGrila(const string &enunt, const list<string> &listaTaguri, const string &raspunsCorect, const int numarOptiuniDeRaspuns, const list<string> &listaOptiuniDeRaspuns) : Intrebare(enunt, listaTaguri, raspunsCorect), numarOptiuniDeRaspuns(numarOptiuniDeRaspuns), listaOptiuniDeRaspuns(listaOptiuniDeRaspuns) {}
+
+    ~IntrebareGrila() {
+        cout << "info: Intrebare grila distrusa\n";
+    }
+
+    void afiseazaIntrebare() const override {
+        cout << idIntrebare << ". " << enunt;
+        char cnt = 'a';
+        for (auto optiuneDeRaspuns : listaOptiuniDeRaspuns) {
+            cout << "\n\t" << cnt++ << " - " << optiuneDeRaspuns;
+        }
+        cout << "\n";
+    }
+};
+
+class IntrebareAdevaratSauFals : public Intrebare {
+private:
+    const bool raspuns;
+protected:
+public:  
+    // IntrebareAdevaratSauFals(const string &enunt, const list<string> &listaTaguri, const string &raspunsCorect, const bool raspuns = "(A/F)") : Intrebare(enunt, listaTaguri, raspunsCorect), raspuns(raspuns) {}
+
+    ~IntrebareAdevaratSauFals() {
+        cout << "info: Intrebare adevarat sau fals distrusa\n";
+    }
+
+    void afiseazaIntrebare() const override {
+        cout << idIntrebare << ". " << enunt << " " << raspuns << "\n";
+    }
+};
+
+class IntrebarePereche : public Intrebare {
+private:
+    const vector<string> multimeA, multimeB;
+protected:
+public:  
+    // IntrebarePereche(const string &enunt, const list<string> &listaTaguri, const string &raspunsCorect, const vector<string> &multimeA, const vector<string> &MultimeB) : Intrebare(enunt, listaTaguri, raspunsCorect), multimeA(multimeA), multimeB(multimeB) {}
+
+    ~IntrebarePereche() {
+        cout << "info: Intrebare pereche distrusa\n";
+    }
+
+    void afiseazaIntrebare() const override {
+        cout << idIntrebare << ". " << enunt;
+        char cntA = 'a';
+        int cntB = 1, limit = 1;
+        for (auto itemA : multimeA) {
+            cout << "\n\t" << cntA++ << ". " << itemA;
+            int cnt = 1;
+            for (auto itemB : multimeB) {
+                if (cnt == limit) {
+                    cout << "\t" << cntB++ << ". " << itemB;
+                    break;
+                }
+                cnt++;
+            }
+            limit++;
+        }
+        cout << "\n";
+    }
+};
+
+class IntrebareLibera : public Intrebare {
+private:
+    const string raspuns;
+protected:
+public:
+    IntrebareLibera(const string &enunt, const list<string> &listaTaguri, const string &raspuns) : Intrebare(enunt, listaTaguri), raspuns(raspuns) {}
+
+    ~IntrebareLibera() {
+        cout << "info: Intrebare pereche distrusa\n";
+    }
+
+    void afiseazaIntrebare() const override {
+        cout << idIntrebare << ". " << enunt << "\n";
+    }
+};
+
+class SingletonMenu {
+private:
+    static SingletonMenu *instanta;
+    vector<Intrebare> *intrebari;
+
+    SingletonMenu() {
+        afiseazaOptiuni();
+    }
+
+    /// Q: De ce nu imi afiseaza mesajul?
+    // ~SingletonMenu() {
+    //     cout << "info: Eliberez memoria din SingletonMenu...\n";
+    //     delete instanta;
+    // }
+protected:
+public:
+    static SingletonMenu* getInstanta() {
+        if (instanta == nullptr) {
+            instanta = new SingletonMenu();
+        }
+        return instanta;
+    }
+
+    void afiseazaOptiuni() const {
+        cout << "Optiuni:\n";
+        cout << "1. Adauga intrebare\n";
+        cout << "2. Afiseaza toate intrebarile\n";
+        cout << "3. Creeaza modul liber de antrenare\n";
+        cout << "4. Creeaza modul de antrenare prin teste\n";
+        cout << "5. Adauga mod de antrenare prin teste\n";
+        cout << "6. Reafiseaza optiunile\n";
+        cout << "7. Iesi din meniu\n";
+    }
+
+    void op1() {
+        string tip;
+        cout << "Adaug intrebare...\n";
+        cout << "Tip (grila / adevarat sau fals / pereche / libera): ";
+        getline(cin, tip);
+
+        if (tip != "grila" && tip != "adevarat sau fals" && tip != "pereche" && tip != "libera") {
+            throw invalid_argument("Tip intrebare invalid\n");
+        } else if (tip == "grila") {
+            // IntrebareGrila(const string &enunt, const list<string> &listaTaguri, const string &raspunsCorect, const int numarOptiuniDeRaspuns, const list<string> &listaOptiuniDeRaspuns)
+            string enunt, raspunsCorect;
+            int numarOptiuniDeRaspuns;
+            list<string> listaTaguri, listaOptiuniDeRaspuns;
+            cout << "Enunt: ";
+            getline(cin, enunt);
+            string raspuns, tag;
+            do {
+                cout << "Tag: ";
+                getline(cin, tag);
+                listaTaguri.push_back(tag);
+                cout << "Inca unul (da/nu): ";
+                getline(cin, raspuns);
+            } while (raspuns == "da");
+            cout << "Raspuns corect: ";
+            getline(cin, raspunsCorect);
+            cout << "Numar optiuni de raspuns: ";
+            cin >> numarOptiuniDeRaspuns;
+            cin.get();
+            char cnt = 'a';
+            string optiuneDeRaspuns;
+            for (int i = 0; i < numarOptiuniDeRaspuns; i++) {
+                cout << cnt++ << ": ";
+                getline(cin, optiuneDeRaspuns);
+                listaOptiuniDeRaspuns.push_back(optiuneDeRaspuns);
+            }
+            // intrebari->push_back(new IntrebareGrila(enunt, listaTaguri, raspunsCorect, numarOptiuniDeRaspuns, listaOptiuniDeRaspuns));
+        } else if (tip == "adevarat sau fals") {
+            /// IntrebareAdevaratSauFals(const string &enunt, const list<string> &listaTaguri, const string &raspunsCorect, const bool raspuns = "(A/F)")
+            string enunt;
+            const list<string> listaTaguri;
+            cout << "Enunt: ";
+            getline(cin, enunt);
+            string raspuns, tag;
+            do {
+                cout << "Tag: ";
+                getline(cin, tag);
+                // listaTaguri.push_back(tag);
+                cout << "Inca unul (da/nu): ";
+                getline(cin, raspuns);
+            } while (raspuns == "da");
+            cout << "Raspuns corect: ";
+            string raspunsCorect;
+            getline(cin, raspunsCorect);
+            // intrebari.push_back(enunt, listaTaguri, raspunsCorect);
+        } else if (tip == "pereche") {
+            /// IntrebarePereche(const string &enunt, const list<string> &listaTaguri, const string &raspunsCorect, const vector<string> &multimeA, const vector<string> &MultimeB)
+
+        } else { /// tip == "libera"
+            /// IntrebareLibera(const string &enunt, listaTaguri, const string &raspuns)
+
+        }
+    }
+
+    void op2() const {
+        cout << "Afisez intrebarile...\n";
+    }
+    
+    void op3() {
+        cout << "Creez modul liber de antrenare...\n";
+    }
+    
+    void op4() {
+        cout << "Creez modul de antrenare prin teste...\n";
+    }
+    
+    void op5() {
+        cout << "Adaug modul de antrenare prin teste...\n";
+    }
+
+    void op6() const {
+        cout << "Reafisez optiunile...\n";
+        afiseazaOptiuni();
+    }
+    
+    void op7() const {
+        cout << "Iesire...\n";
+    }
+};
+
+SingletonMenu* SingletonMenu::instanta = nullptr;
+
+int main() {
+    SingletonMenu *m = SingletonMenu::getInstanta();
+    int optiune;
+
+    try {
+        do {
+            cout << "Optiune: ";
+            cin >> optiune;
+            cin.get();
+
+            if (optiune < 1 || optiune > 7) {
+                throw out_of_range("Optiune invalida\n");
+            }
+
+            switch (optiune) {
+                case 1: m->op1(); break;
+                case 2: m->op2(); break;
+                case 3: m->op3(); break;
+                case 4: m->op4(); break;
+                case 5: m->op5(); break;
+                case 6: m->op6(); break;
+                case 7: m->op7(); break;
+            }
+        } while (optiune != 7);
+    } catch (const out_of_range &e) {
+        cout << "Out of range exception: " << e.what();
+    } catch (const invalid_argument &e) {
+        cout << "Invalid argument exception: " << e.what();
+    }
+
+    return 0;
+}
--- e.cpp
+++ e.cpp
@@ -3,30 +3,9 @@
 using namespace std;
 
 int main() {
-    int x = 10;
-    int *p = &x;
-
-    cout << p << "\n"; /// 0x61ff08
-    cout << ++p << "\n"; /// 0x61ff0c
-    cout << --p << "\n"; /// 0x61ff08
-    cout << p + 0 << "\n"; /// 0x61ff08
-    cout << p + 1 << "\n"; /// 0x61ff0c
-    cout << p + 2 << "\n"; /// 0x61ff10
-    cout << p + 3 << "\n"; /// 0x61ff14
-    cout << p + 4 << "\n"; /// 0x61ff18
-    cout << p + 5 << "\n"; /// 0x61ff1c
-    cout << p + 6 << "\n"; /// 0x61ff20
-    cout << p + 7 << "\n"; /// 0x61ff24
-    /// Deci daca p = 0x61ff08 atunci p+7 = 0x61ff08 + (4*7)16 = 0x61ff08 + (28)16 = 0x61ff08 + (0001 1100)2 = 0x61ff08 + 0x00001c = 0x61ff08 (explicatie: luam 8 si c, 8 + 12 = 20 = (1 0100)2 = (14)16, deci scriem 4 la final si pastram 1 in minte, trecem la 0 si 1, 0 + 1 = 1 dar mai avem 1 in minte, deci avem 2 = (2)16, restul ramane la fel, ca aveam numai zero)
-
-    int x2 = 11;
-    int *p2 = &x2;
-    cout << p << " - " << p2 << " = ";
-    cout << p - p2 << "\n"; /// 0x61ff04 - 0x61ff00 = 1
-
-    cout << "p == p2 is " << (p == p2) << "\n"; /// 0
-    cout << "p < p2 is " << (p < p2) << "\n"; /// 0
-    cout << "p > p2 is " << (p > p2) << "\n"; /// 1
+    int a = 1, b = 2;
+    const int * const p = &a;
+    *p = 3;
 
     return 0;
 }
