2534228774a57e900e54f5fc3701d946784b42d2
INDavid04 <davidirimia04@gmail.com>
2025-08-21 13:44:22 +0300

    feat: barem and destructor call

--- colocviu2014septembrie.cpp
+++ colocviu2014septembrie.cpp
@@ -93,7 +93,7 @@ public:
 
 class AngajatCuOra : public Angajat {
 private:
-    const string dataIntrareInVigoareContract, dataTerminareContract;
+    const string dataTerminareContract;
 protected:
 public:
     AngajatCuOra(const string &nume, const string &prenume, const string &tipContract, const string &dataIntrareInVigoareContract, const string &dataTerminareContract) : Angajat(nume, prenume, tipContract, dataIntrareInVigoareContract), dataTerminareContract(dataTerminareContract) {
@@ -119,16 +119,18 @@ private:
     }
 protected:
 public:
-    void elibereazaMemoria() {
+    /// AFTER: destructor
+    ~SingletonMenu() {
         for (auto *angajat : angajati) {
             delete angajat;
             angajat = nullptr;
         }
 
-        delete instanta;
-        instanta = nullptr;
+        /// AFTER: os delete it
+        // delete instanta;
+        // instanta = nullptr;
 
-        cout << "Memoria a fost eliberata\n";
+        cout << "Meniu distrus. Memoria a fost eliberata\n";
     }
 
     void afiseazaOptiuni() const {
@@ -239,9 +241,42 @@ int main() {
         cout << "Out of range: " << e.what();
     }
 
-    m->elibereazaMemoria();
+    /// Apelam destructorul, ~SingletonMenu(), care va elibera memoria
+    delete m;
 
     return 0;
 }
 
 /// Q: De ce nu se apeleaza destructorul ~SingletonMenu?
+/// A: Nu este definit :) In plus, nu am sters pointerul la finalul functiei main, acel `delete m;`
+
+// - Barem 2014 septembrie
+//     - [x]  1p - oficiu (sursa compileaza)
+//     - [x]  2p - ierarhie (identificarea claselor + atributelor)
+//     - [x]  0.25p - const (pentru atribute, functii sau argumente la functii)
+//     - [x]  0.25p - static (functii si atribute statice)
+//     - [x]  0.25p - destructor virtual (folosit in toate locurile in care este necesar)
+//     - [x]  0.25p - mostenire (reutilizare de cod, ex: apeleaza o functie a clasei de baza in momentul rescrierii ei in clasa derivata)
+//     - [x]  0.25p - abstract (exista o clasa abstracta)
+//     - [x]  0.25p - STL containers (vector, list, map, etc.)
+//     - [ ]  0.25p - STL alte elemente (sort, transform, swap)
+//     - [x]  0.25p - exceptia se propaga (nu poate fi tratata in acelasi loc de locul unde a fost aruncata)
+//     - [x]  0.25p - exceptia este prinsa (tratarea cu sens a unei exceptii aruncate)
+//     - [ ]  0.25p - RTTI (dynamic_cast, type_info)
+//     - [x]  0.25p - clasa Singleton
+//     - [ ]  0.25p - clasa Factory
+//     - [x]  0.25p - ID comanda incrementat automat in constructor
+//     - [ ]  0.5p - bonus pentru folosirea conceptelor POO care nu sunt in barem, precum template
+//     - [ ]  -0.5p - depunctari, de exemplu: memory leaks
+//     - [ ]  0.25p - specific 2025-06-02, existenta unei functii ce calculeaza energia necesara pentru un produs (functie virtuala, rescrisa corespunzator in derivate)
+//     - [ ]  0.25p - specific 2025-06-02, existenta unei functii care calculeaza energia necesara pentru o comanda (exista clasa Comanda)
+//     - [ ]  0.25p - specific 2025-06-02, validarea tipului unui desert (in constructor se valideaza ca tipul este doar cel prestabilit, punctat si enum)
+//     - [ ]  1p - specific 2025-06-02, identificare comportament de baza al clasei angajat (minim, functii care intorc costul de baza pentru preparare, livrare, comandare, recuperare; alte functii precum cele care verifica daca o actiune poate fi executata)
+//     - [ ]  0.75p - specific 2025-06-02, identificare comportament clase derivate ale clasei angajat (rescrie functiile in clasele derivate ca sa fie evidentiat comportamentul specific al derivatelor, ex: rescrie functia de recuperare in clasa Bucatar)
+//     - [ ]  0.5p - specific 2025-06-02, asamblare comportament Angajat (functii care imbina comportament de baza + verificari necesare ca sa creeze clasa Angajat)
+//     - [ ]  0.5p - specific 2025-06-02, existenta unui meniu cu produse predefinite
+//     - [ ]  0.5p - specific 2025-06-02, afisarea numarului de angajati de fiecare tip (doar daca e folosit dynamic_cast sau type_info)
+//     - [ ]  1p - specific 2025-06-02, implementare ciclu dat ca exemplu
+//     - [ ]  1p - specific 2025-06-02, prioritizare comanda (pot introduce un ID al unei comenzi care va prioritiza o anumita comanda a fi preparata/livrata prima)
+//     - [ ]  0.5p - specific 2025-06-02, optimizarea ciclului (aloca actiunile specifice catre angajatii cu o anumita functie)
+//     - [ ]  1p - specific 2025-06-02, strategia de alegere a comenzilor care sunt preparate
