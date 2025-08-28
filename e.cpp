1a6da8981f5dce9be899ef52e953b90b51b7b417
INDavid04 <davidirimia100@gmail.com>
2025-08-28 16:50:29 +0300

    feat: understand parameters

--- e.cpp
+++ e.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-/// Functie care verifica apartenta unei valori intr-un vector
-void cauta(vector<int> numere, int valoare) {
-    if (find(numere.begin(), numere.end(), valoare) != numere.end()) {
-        cout << "Am gasit " << valoare << "\n";
-    } else {
-        cout << "Nu am gasit " << valoare << "\n";
-    }
-}
+void f(int x) { x = x *2; } /// prin valoare
+void g(int *x) { *x = *x + 30; } ///  prin pointer
+void h(int &x) { x = x + 50; } /// prin referinta
 
 int main() {
-    vector<int> numere = {1, 2, 3, 4};
-
-    /// 1. Cauta o valoare intr-un vector
-    cauta(numere, 0);
-    cauta(numere, 1);
-    cauta(numere, 2);
-    cauta(numere, 3);
-    cauta(numere, 4);
-    cauta(numere, 5);
-
-    /// 2. Modifica 3 in 33 daca 3 se afla in vector
-    auto it = find(numere.begin(), numere.end(), 3);
-    if (it != numere.end()) {
-        *it = 33;
-    }
-
-    /// 3. Afiseaza vectorul
-    for (auto numar : numere) {
-        cout << numar << " ";
-    }
-    cout << "\n";
+    int x = 10;
+    f(x);
+    cout<<"x = "<<x<<endl; /// 10
+    g(&x);
+    cout<<"x = "<<x<<endl; /// 40
+    h(x);
+    cout<<"x = "<<x<<endl; /// 90
     
     return 0;
 }
